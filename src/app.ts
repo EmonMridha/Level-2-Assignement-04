443c000a8c2c192d1e8f3aa318de5e940925c777
EmonMridha <emonmridha712@gmail.com>
2026-07-18 13:21:22 +0600

    added all the models

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import cookieParser from "cookie-parser"
 import cors from 'cors'
 import config from "./config";
+import { prisma } from "./lib/prisma";
 
 const app: Application = express();
 
@@ -13,7 +14,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
+    const user = await prisma.user.findMany();
+    console.log(user);
     res.send("Hello, world!");
 })
 
